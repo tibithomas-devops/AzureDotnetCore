941230ce8a103e6441d31a9c07f1b88ffe81283b
tibithomas-devops <81287153+tibithomas-devops@users.noreply.github.com>
2021-03-24 13:49:15 +0530

    Uploaded Hello World Application

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Hello";
+            string str2 = "World";
+            Console.Write(str1 + str2);
+            Console.ReadKey();
+        }
+    }
+}
